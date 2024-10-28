b09be8680ae0f723cd25d25759db13a0359ffe30
Ritesh2006M <riteshreddy65@gmail.com>
2024-10-28 22:33:14 +0530

    "minor"

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
-import Login from "../../components/login/index";
 import "./style.css";
 import Image from "./homePageImage.jpg";
 
